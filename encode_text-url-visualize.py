26f43a31c3cce7d727253d9f4beabbeef8a6475a
Tsubasa Kato <tsubasa@superai.online>
2021-12-10 09:37:07 +0900

    Created encode_text-url-visualize.py

---
+++ encode_text-url-visualize.py
@@ -0,0 +1,40 @@
+#Based on code from: https://machinelearningmastery.com/prepare-text-data-machine-learning-scikit-learn/
+#This code visualizes text and URL pair as a vector with matplotlib.
+#Original code by Jason Brownlee, code edited by Tsubasa Kato (@_stingraze on Twitter) on December 10th, 2021
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD
+import matplotlib.pylab as plt
+import scipy.sparse as sparse
+
+# list of text documents
+text = ["The quick brown fox jumped over the lazy dog."]
+url = ["https://www.google.com"]
+text2 = ["The red brown fox jumped over the fast dog."]
+url2 = ["https://www.superai.online"]
+# create the transform
+vectorizer = CountVectorizer()
+vectorizer2 = CountVectorizer()
+# tokenize and build vocab
+text = text  + list(url)
+text2 = text2 + list(url2)
+vectorizer.fit(text)
+vectorizer2.fit(text2)
+# summarize
+print(vectorizer.vocabulary_)
+print(vectorizer2.vocabulary_)
+# encode document
+vector = vectorizer.transform(text)
+vector2 = vectorizer.transform(text2)
+# summarize encoded vector
+print(vector.shape)
+print(type(vector))
+print(vector.toarray())
+# summarize encoded vector2
+print(vector2.shape)
+print(type(vector2))
+print(vector2.toarray())
+
+plt.spy(vector)
+plt.figure(0)
+plt.spy(vector2)
